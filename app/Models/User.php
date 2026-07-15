bd24fd968eecfe24605d9a206f6e1f07b3db5514
46792543-prog <46792543@populorumjujuy.ar>
2026-07-15 03:07:29 -0300

    Update User.php

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -18,9 +19,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'dni',
+        'nombre',
+        'apellido',
+        'fecha_nacimiento',
+        'telefono',
+        'direccion',
+        'localidad',
         'email',
         'password',
+        'rol',
     ];
 
     /**
@@ -40,5 +48,51 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'fecha_nacimiento' => 'date',
     ];
+
+    public function inscripcionesCarrera(): HasMany
+    {
+        return $this->hasMany(InscripcionCarrera::class);
+    }
+
+    public function documentosAlumno(): HasMany
+    {
+        return $this->hasMany(DocumentoAlumno::class);
+    }
+
+    public function cuotas(): HasMany
+    {
+        return $this->hasMany(Cuota::class);
+    }
+
+    public function historialMaterias(): HasMany
+    {
+        return $this->hasMany(HistorialMateria::class);
+    }
+
+    public function inscripcionesMesa(): HasMany
+    {
+        return $this->hasMany(InscripcionMesa::class);
+    }
+
+    public function esAlumno(): bool
+    {
+        return $this->rol === 'alumno';
+    }
+
+    public function esSecretario(): bool
+    {
+        return $this->rol === 'secretario';
+    }
+
+    public function esDirector(): bool
+    {
+        return $this->rol === 'director';
+    }
+
+    public function esStaff(): bool
+    {
+        return in_array($this->rol, ['secretario', 'director']);
+    }
 }
