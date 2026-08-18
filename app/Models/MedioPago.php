73c2a6cb761d5efc9e115c73cd0732ef56a963d7
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:55:15 -0300

    Create MedioPago.php

---
+++ app/Models/MedioPago.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MedioPago extends Model
+{
+    protected $table = 'medio_pago';
+    protected $primaryKey = 'id_medio_pago';
+
+    protected $fillable = ['nombre_medio'];
+}
