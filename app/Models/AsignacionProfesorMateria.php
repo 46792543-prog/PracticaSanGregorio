5ea471e0987f76776b316f632a9c1b4da040df4b
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:53:21 -0300

    Update AsignacionProfesorMateria.php

--- app/Models/AsignacionProfesorMateria.php
+++ app/Models/AsignacionProfesorMateria.php
@@ -4,38 +4,54 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AsignacionProfesorMateria extends Model
 {
-    protected $table = 'asignaciones_profesor_materia';
+    protected $table = 'asignacion_profesor_materia';
+    protected $primaryKey = 'id_asignacion';
 
     protected $fillable = [
-        'profesor_id',
-        'materia_id',
-        'anio_lectivo_id',
-        'hora_inicio',
-        'hora_fin',
+        'id_profesor',
+        'id_materia',
+        'id_anio_lectivo',
+        'fecha_asignacion',
+        'id_anio_cursada',
+        'id_turno_cursada',
         'aula',
-        'turno',
-        'dias_cursada',
     ];
 
     protected $casts = [
-        'dias_cursada' => 'array',
+        'fecha_asignacion' => 'date',
     ];
 
     public function profesor(): BelongsTo
     {
-        return $this->belongsTo(Profesor::class);
+        return $this->belongsTo(Profesor::class, 'id_profesor', 'id_profesor');
     }
 
     public function materia(): BelongsTo
     {
-        return $this->belongsTo(Materia::class);
+        return $this->belongsTo(Materia::class, 'id_materia', 'id_materia');
     }
 
     public function anioLectivo(): BelongsTo
     {
-        return $this->belongsTo(AnioLectivo::class);
+        return $this->belongsTo(AnioLectivo::class, 'id_anio_lectivo', 'id_anio_lectivo');
+    }
+
+    public function anioCursada(): BelongsTo
+    {
+        return $this->belongsTo(AnioCursada::class, 'id_anio_cursada', 'id_anio_cursada');
+    }
+
+    public function turnoCursada(): BelongsTo
+    {
+        return $this->belongsTo(TurnoCursada::class, 'id_turno_cursada', 'id_turno_cursada');
+    }
+
+    public function horarios(): HasMany
+    {
+        return $this->hasMany(HorarioAsignacion::class, 'id_asignacion', 'id_asignacion');
     }
 }
