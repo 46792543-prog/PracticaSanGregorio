21fa2401e1b2a452adefc7d8314498eeaeee5d23
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:53:18 -0300

    Update AnioLectivo.php

--- app/Models/AnioLectivo.php
+++ app/Models/AnioLectivo.php
@@ -3,34 +3,46 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class AnioLectivo extends Model
 {
-    protected $table = 'anios_lectivos';
+    protected $table = 'anio_lectivo';
+    protected $primaryKey = 'id_anio_lectivo';
 
     protected $fillable = [
         'anio',
-        'estado',
+        'id_estado_anio',
     ];
+
+    public function estadoAnio(): BelongsTo
+    {
+        return $this->belongsTo(EstadoAnioLectivo::class, 'id_estado_anio', 'id_estado_anio');
+    }
 
     public function inscripcionesCarrera(): HasMany
     {
-        return $this->hasMany(InscripcionCarrera::class);
+        return $this->hasMany(InscripcionCarrera::class, 'id_anio_lectivo', 'id_anio_lectivo');
     }
 
     public function cuotas(): HasMany
     {
-        return $this->hasMany(Cuota::class);
+        return $this->hasMany(CuotaAlumno::class, 'id_anio_lectivo', 'id_anio_lectivo');
     }
 
-    public function historialMaterias(): HasMany
+    public function historialAlumno(): HasMany
     {
-        return $this->hasMany(HistorialMateria::class);
+        return $this->hasMany(HistorialAlumno::class, 'id_anio_lectivo', 'id_anio_lectivo');
     }
 
     public function mesasExamen(): HasMany
     {
-        return $this->hasMany(MesaExamen::class);
+        return $this->hasMany(MesaExamen::class, 'id_anio_lectivo', 'id_anio_lectivo');
+    }
+
+    public function asignaciones(): HasMany
+    {
+        return $this->hasMany(AsignacionProfesorMateria::class, 'id_anio_lectivo', 'id_anio_lectivo');
     }
 }
