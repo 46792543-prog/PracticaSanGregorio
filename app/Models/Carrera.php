243d9e78193170293b5b397033d4ea6357db186e
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:53:26 -0300

    Update Carrera.php

--- app/Models/Carrera.php
+++ app/Models/Carrera.php
@@ -4,32 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Carrera extends Model
 {
     use HasFactory;
 
+    protected $table = 'carrera';
+    protected $primaryKey = 'id_carrera';
+
     protected $fillable = [
-        'nombre',
+        'nombre_carrera',
         'familia_profesional',
-        'duracion_anios',
+        'duracion_anos',
         'resolucion_ministerial',
-        'estado',
+        'id_estado_carrera',
     ];
+
+    public function getNombreAttribute()
+    {
+        return $this->nombre_carrera;
+    }
+
+    public function estadoCarrera(): BelongsTo
+    {
+        return $this->belongsTo(EstadoCarrera::class, 'id_estado_carrera', 'id_estado_carrera');
+    }
 
     public function materias(): HasMany
     {
-        return $this->hasMany(Materia::class);
+        return $this->hasMany(Materia::class, 'id_carrera', 'id_carrera');
     }
 
     public function inscripcionesCarrera(): HasMany
     {
-        return $this->hasMany(InscripcionCarrera::class);
+        return $this->hasMany(InscripcionCarrera::class, 'id_carrera', 'id_carrera');
     }
 
     public function documentoRequisitos(): HasMany
     {
-        return $this->hasMany(DocumentoRequisito::class);
+        return $this->hasMany(DocumentoRequisito::class, 'id_carrera', 'id_carrera');
     }
 }
