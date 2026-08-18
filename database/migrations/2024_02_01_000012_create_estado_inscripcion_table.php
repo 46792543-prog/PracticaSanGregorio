e5b5d56e4a8ec40973ba93201f915a1419d17651
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:40:37 -0300

    Create 2024_02_01_000012_create_estado_inscripcion_table.php

---
+++ database/migrations/2024_02_01_000012_create_estado_inscripcion_table.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('estado_inscripcion', function (Blueprint $table) {
+            $table->id('id_estado_inscripcion');
+            $table->string('nombre_estado', 25)->unique();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('estado_inscripcion');
+    }
+};
