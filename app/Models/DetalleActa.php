01aefa763fbb9637f502bd5a0ddcf687086464bd
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:54:00 -0300

    Update DetalleActa.php

--- app/Models/DetalleActa.php
+++ app/Models/DetalleActa.php
@@ -8,29 +8,30 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class DetalleActa extends Model
 {
     protected $table = 'detalle_acta';
+    protected $primaryKey = 'id_detalle';
 
     protected $fillable = [
-        'acta_id',
-        'user_id',
+        'id_acta',
+        'id_persona_alumno',
         'nota_escrito',
         'nota_oral',
-        'nota_promedio',
+        'nota_final',
         'resultado',
     ];
 
     protected $casts = [
         'nota_escrito' => 'decimal:2',
         'nota_oral' => 'decimal:2',
-        'nota_promedio' => 'decimal:2',
+        'nota_final' => 'decimal:2',
     ];
 
     public function acta(): BelongsTo
     {
-        return $this->belongsTo(Acta::class);
+        return $this->belongsTo(Acta::class, 'id_acta', 'id_acta');
     }
 
-    public function user(): BelongsTo
+    public function personaAlumno(): BelongsTo
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(Persona::class, 'id_persona_alumno', 'id_persona');
     }
 }
