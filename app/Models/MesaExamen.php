3751f232aaf896cbb48b13b016e9bd20866368ef
46792543-prog <46792543@populorumjujuy.ar>
2026-08-18 15:59:00 -0300

    Update MesaExamen.php

--- app/Models/MesaExamen.php
+++ app/Models/MesaExamen.php
@@ -9,17 +9,19 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class MesaExamen extends Model
 {
-    protected $table = 'mesas_examen';
+    protected $table = 'mesa_examen';
+    protected $primaryKey = 'id_mesa';
 
     protected $fillable = [
-        'materia_id',
-        'anio_lectivo_id',
-        'turno',
-        'llamado',
+        'id_materia',
+        'id_anio_lectivo',
+        'id_turno',
+        'id_llamado',
         'fecha_examen',
         'fecha_inicio_inscripcion',
         'fecha_fin_inscripcion',
-        'estado',
+        'id_estado_mesa',
+        'cupo_maximo',
     ];
 
     protected $casts = [
@@ -30,26 +32,41 @@ class MesaExamen extends Model
 
     public function materia(): BelongsTo
     {
-        return $this->belongsTo(Materia::class);
+        return $this->belongsTo(Materia::class, 'id_materia', 'id_materia');
     }
 
     public function anioLectivo(): BelongsTo
     {
-        return $this->belongsTo(AnioLectivo::class);
+        return $this->belongsTo(AnioLectivo::class, 'id_anio_lectivo', 'id_anio_lectivo');
+    }
+
+    public function turnoExamen(): BelongsTo
+    {
+        return $this->belongsTo(TurnoExamen::class, 'id_turno', 'id_turno');
+    }
+
+    public function llamadoExamen(): BelongsTo
+    {
+        return $this->belongsTo(LlamadoExamen::class, 'id_llamado', 'id_llamado');
+    }
+
+    public function estadoMesa(): BelongsTo
+    {
+        return $this->belongsTo(EstadoMesa::class, 'id_estado_mesa', 'id_estado_mesa');
     }
 
     public function inscripciones(): HasMany
     {
-        return $this->hasMany(InscripcionMesa::class);
+        return $this->hasMany(InscripcionMesa::class, 'id_mesa', 'id_mesa');
     }
 
     public function tribunal(): HasMany
     {
-        return $this->hasMany(TribunalMesa::class);
+        return $this->hasMany(TribunalMesa::class, 'id_mesa', 'id_mesa');
     }
 
     public function acta(): HasOne
     {
-        return $this->hasOne(Acta::class);
+        return $this->hasOne(Acta::class, 'id_mesa', 'id_mesa');
     }
 }
